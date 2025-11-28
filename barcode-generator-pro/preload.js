cb2093fbea179d2eb975c50e18a0ac1a7e4f5d92
gilocie <84890246+gilocie@users.noreply.github.com>
2025-11-28 17:54:33 +0200

    Delete barcode-generator-pro directory

--- barcode-generator-pro/preload.js
+++
@@ -1,7 +0,0 @@
-// preload.js — exposes a small safe API to renderer
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  chooseFolder: () => ipcRenderer.invoke('show-folder-picker'),
-  saveFile: (args) => ipcRenderer.invoke('save-file', args)
-});
